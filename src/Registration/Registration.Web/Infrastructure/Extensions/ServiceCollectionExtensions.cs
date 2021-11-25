884d106fcff4eb6c59eb6f34f06b2fab415db597
zizoo <aav1984@gmail.com>
2021-11-25 21:16:37 +0700

    change cnfig

---
+++ src/Registration/Registration.Web/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Registration.DataAccess;
+
+namespace Registration.Web.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRegistrationApplicationServices(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+    }
+}
--- src/Registration/Registration.Web/Startup.cs
+++ src/Registration/Registration.Web/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Registration.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace Registration.Web
 {
@@ -24,6 +26,9 @@ namespace Registration.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
